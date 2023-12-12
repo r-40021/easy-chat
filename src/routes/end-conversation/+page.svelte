24489d78302fa7477042df61d5c564321b935acb
FrogApp <frog@frogapp.net>
2023-12-12 19:30:09 +0900

    Initial commit

---
+++ src/routes/end-conversation/+page.svelte
@@ -0,0 +1,74 @@
+<svelte:head>
+  <title>会話を終わらせる - チャット返信文作成支援ツール</title>
+</svelte:head>
+
+<h1 class="text-center font-semibold text-3xl">
+  会話を終わらせることを検討してみてください
+</h1>
+
+<div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 mt-8">
+  <div class="max-w-full bg-white border border-gray-200 rounded-lg shadow">
+    <img class="rounded-t-lg" src="" alt="" />
+    <div class="p-5">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        無言で終わらせる
+      </h5>
+      <p class="font-normal text-gray-700">
+        相手が返信っぽいものをしている場合は、無言で会話を終わらせるのが自然です。
+      </p>
+    </div>
+  </div>
+
+  <div class="max-w-full bg-white border border-gray-200 rounded-lg shadow">
+    <img class="rounded-t-lg" src="" alt="" />
+    <div class="p-5">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        一言だけ返信をする
+      </h5>
+      <p class="mb-3 font-normal text-gray-700">
+        「笑」、「草」、「おーん」など、単純な返信をして会話を終わらせることもできます。
+      </p>
+      <p class="font-normal text-gray-700">
+        ※相手によっては冷淡だと感じられてしまうこともあるので注意が必要です。
+      </p>
+    </div>
+  </div>
+
+  <div class="max-w-full bg-white border border-gray-200 rounded-lg shadow">
+    <img class="rounded-t-lg" src="" alt="" />
+    <div class="p-5">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        スタンプ・リアクションを返す
+      </h5>
+      <p class="mb-3 font-normal text-gray-700">
+        手軽な方法ですが、他の2つよりも温かみを演出することができます。
+      </p>
+      <p class="font-normal text-gray-700">
+        悩んだらこの方法を選ぶのが無難です。
+      </p>
+    </div>
+  </div>
+</div>
+
+<h1 class="text-center font-semibold text-3xl mt-20">
+  余裕があれば、新しい話題を振ってみると良い雰囲気になります
+</h1>
+
+<p class="mt-2">
+  最近あった面白いこと、今週末の予定、最近の天気、最近食べた美味しかったもの……など、気軽に話せる話題を振ってみましょう。
+</p>
+<p>
+  え？話題が見つからない…？そんなときは<a
+    href="https://hothukurou.com/game/Word/index.html"
+    class="font-medium text-blue-600 underline hover:no-underline"
+    target="_blank"
+    rel="noopener noreferrer">ルーレット</a
+  >を使うと楽です。
+</p>
+<a
+  type="button"
+  href="/"
+  class="text-white text-base font-medium bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 me-2 mb-2 mt-12"
+>
+  ホームへ戻る
+</a>
---
+++ src/routes/step1/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+  let show = false;
+  let reply = '';
+  function handleClick() {
+    show = true;
+  }
+  function handleInput() {
+    localStorage.setItem('reply', reply);
+  }
+</script>
+
+<h1 class="text-center font-semibold text-3xl">
+  まずは、とりあえず返信文を作ってみてください
+</h1>
+<p class="mt-2">
+  ここで入力した返信文をこの後の画面でブラッシュアップしていきます。現段階では言いたいことが伝われば
+  OK です。
+</p>
+
+<form class="w-full lg:w-9/12">
+  <div class="w-full mb-4 mt-8 border border-gray-200 rounded-lg bg-gray-50">
+    <div class="px-4 py-2 bg-white rounded-t-lg">
+      <label for="reply" class="sr-only">返信文</label>
+      <textarea
+        id="reply"
+        rows="8"
+        class="w-full px-0 text-sm text-gray-900 bg-white border-1"
+        placeholder="クリックして入力"
+        on:change={handleInput}
+        bind:value={reply}
+        required
+      ></textarea>
+    </div>
+    <div class="flex items-center justify-end px-3 py-2 border-t">
+      <a
+        href="/step2"
+        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800"
+      >
+        次へ
+        <svg
+          class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </a>
+    </div>
+  </div>
+</form>
+
+<button
+  type="submit"
+  on:click={handleClick}
+  class:hidden={show === true}
+  class="inline-flex items-center py-2.5 px-4 mt-8 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800"
+>
+  どうしても思い浮かばない場合
+</button>
+
+<div class:hidden={show === false} class="mt-8">
+  <p>
+    どうしても返信文が思い浮かばない場合は、<a
+      href="https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx"
+      class="font-medium text-blue-600 underline hover:no-underline"
+      target="_blank"
+      rel="noopener noreferrer">奥の手</a
+    >を使う方法もあります。
+  </p>
+  <p class="mt-3">プロンプトの例:</p>
+  <p class="mb-3 p-3 border-gray-400 border-solid border">
+    (相手との関係 (友人、家族など))
+    とチャットを行っており、以下のようなメッセージが送られてきました。この後に続く適切な返答を考えてください。
+    <br /><br />
+    (相手から送られてきたメッセージ)
+  </p>
+  <p>※「創造的」モードを選ぶと良い返答が得られます。</p>
+</div>
