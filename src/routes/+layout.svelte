c6e8f2ce54bcecf8486890019f8149fb90093dfd
FrogApp <frog@frogapp.net>
2023-12-12 19:30:09 +0900

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+  import "../app.css";
+</script>
+
+<nav class="border-solid border-gray-200 border-b mb-6">
+  <div
+    class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+  >
+    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <span class="self-center text-2xl whitespace-nowrap"
+        >チャット返信文作成支援ツール</span
+      >
+    </a>
+  </div>
+</nav>
+
+<div class="container mx-auto">
+  <div class="flex flex-col justify-center items-center mt-10">
+    <slot />
+  </div>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<svelte:head>
+  <title>チャット返信文作成支援ツール</title>
+</svelte:head>
+
+<h1 class="text-center font-semibold text-3xl">
+  現在の状況について、以下の項目に当てはまるものはありますか？
+</h1>
+<ul class="max-w-md space-y-1 text-gray-700 text-xl list-inside mt-6">
+  <li class="flex items-center">
+    <svg
+      class="w-3.5 h-3.5 me-2 text-green-500 flex-shrink-0"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+    >
+      <path
+        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+      />
+    </svg>
+    相手が乗り気ではなさそう
+  </li>
+  <li class="flex items-center">
+    <svg
+      class="w-3.5 h-3.5 me-2 text-green-500 flex-shrink-0"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+    >
+      <path
+        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+      />
+    </svg>
+    話が噛み合ってなさそう
+  </li>
+  <li class="flex items-center">
+    <svg
+      class="w-3.5 h-3.5 me-2 text-green-500 flex-shrink-0"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+    >
+      <path
+        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"
+      />
+    </svg>
+    相手の返信が極端に短い
+  </li>
+</ul>
+<div class="btns">
+  <a
+    type="button"
+    href="/end-conversation"
+    class="text-white text-base font-medium bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 me-2 mb-2 mt-6"
+  >
+    ある
+  </a>
+  <a
+    type="button"
+    href="/step1"
+    class="text-gray-900 text-base font-medium bg-gray-200 border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-3 me-2 mb-2"
+  >
+    ない
+  </a>
+</div>
