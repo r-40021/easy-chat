4af8cf291c8ccee8c21fcf1adb41200158728477
FrogApp <frog@frogapp.net>
2023-12-14 01:02:53 +0900

    Add step2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,22 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+JP:wght@400;600&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <nav class="border-solid border-gray-200 border-b mb-6">
   <div
     class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
   >
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
       <span class="self-center text-2xl whitespace-nowrap"
-        >チャット返信文作成支援ツール</span
+        >チャット返信作成支援ツール</span
       >
     </a>
   </div>
