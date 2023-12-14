ad49bb210d88b98a7e106b16c6fdccf794fc72f6
FrogApp <frog@frogapp.net>
2023-12-14 19:27:31 +0900

    Add db

--- src/routes/refine/synonyms.svelte
+++ src/routes/refine/synonyms.svelte
@@ -2,7 +2,7 @@
   export let reply = '';
 </script>
 
-<form class="w-screen lg:w-[75vw]">
+<form action="/refine/synonyms" method="post" class="w-screen lg:w-[75vw]">
   <label
     for="default-search"
     class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
@@ -31,6 +31,7 @@
     <input
       type="search"
       id="default-search"
+      name="word"
       class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       placeholder="類義語を検索..."
       required
