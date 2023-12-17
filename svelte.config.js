904f980a553cacd2716b832b6b757c21db0b52a2
FrogApp <frog@frogapp.net>
2023-12-18 00:31:44 +0900

    Add analytics

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		csp: {
+			directives: {
+				'script-src': ['self', 'https://static.cloudflareinsights.com', 'https://cloudflareinsights.com'],
+				'connect-src': ['self', 'https://static.cloudflareinsights.com', 'https://cloudflareinsights.com'],
+			}
+		}
 	}
 };
 
